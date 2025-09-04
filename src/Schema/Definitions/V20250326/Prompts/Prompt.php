0be95369e59226560470cae74706fd9ffb88a87a
Angel Gonzalez <usftechacg@gmail.com>
2025-09-04 14:22:57 -0400

    v0
    Removal of any code from the main branch

--- src/Schema/Definitions/V20250326/Prompts/Prompt.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Superconductor\Schema\Definitions\V20250326\Prompts;
-
-use Superconductor\Schema\Definitions\V20241105\Prompts\Prompt as PreviousVersion;
-
-class Prompt extends PreviousVersion
-{
-}
